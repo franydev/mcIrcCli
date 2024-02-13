aa53a6ede2bc6244d42bec5d5c23526836d6e8dc
frany <frany@frany.dev>
2024-02-13 00:39:29 -0300

    Update eeprom.lua

--- eeprom.lua
+++ eeprom.lua
@@ -1,7 +1,7 @@
 local io = {}
 function io.stream(a,b,c)checkArg(1,a,'number')checkArg(2,b,"table","string","nil")assert(a>=0,'fd must be >= 0. 0 is input, 1 is stdout, 2 is stderr')local d=require("process").info().data.io;if b then if type(b)=="string"then b=assert(io.open(b,c))end;d[a]=b end;return d[a]end;function io.output(a)return io.stream(1,a,'w')end;function io.write(...)return io.output():write(...)end;
 
-print("Hello World!")
+io.write("Hello World!")
 
 while true do
 end
